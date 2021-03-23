4c97b7a8ae685fe888b8b35aedbd3d2027f089fa
Giorgio <giorgio.pignattari@gmail.it>
2021-03-23 16:15:52 +0100

    html ok, inizio js

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// <!-- Descrizione
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data. -->
+
+var app = new Vue(
+  {
+    
+  }
+)
