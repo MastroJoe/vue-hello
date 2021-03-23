8efbb320d364e86b9528a3fd347911397f32f73c
Giorgio <giorgio.pignattari@gmail.it>
2021-03-23 16:26:38 +0100

    messaggio stampato su HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,13 @@
 // Bonus
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data. -->
 
-var app = new Vue(
-  {
-    
-  }
-)
+$(document).ready(function(){
+  var app = new Vue(
+    {
+      el: '#root',
+      data: {
+        message: 'Hello World :)',
+      }
+    }
+  );
+});
